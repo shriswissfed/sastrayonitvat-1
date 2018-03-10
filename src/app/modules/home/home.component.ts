c694f2e35e46ade0238fad3414f1781f60397315
Aranganathan <arangates@gmail.com>
2018-03-11 00:30:28 +0100

    test 01

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -11,9 +11,9 @@ export class HomeComponent implements OnInit , OnDestroy {
   themeClass: any;
   mobileQuery: MediaQueryList;
 
-  fillerNav = Array(50).fill(0).map((_, i) => `Nav Item ${i + 1}`);
+  fillerNav = Array(10).fill(0).map((_, i) => `Nav Item ${i + 1}`);
 
-  fillerContent = Array(50).fill(0).map(() =>
+  fillerContent = Array(10).fill(0).map(() =>
     `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
@@ -22,7 +22,9 @@ export class HomeComponent implements OnInit , OnDestroy {
 
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private overlayContainer: OverlayContainer) {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
+     private overlayContainer: OverlayContainer,
+    ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
